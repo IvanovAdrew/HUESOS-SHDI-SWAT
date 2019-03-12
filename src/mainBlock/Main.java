b27910bdd1c158d47987309e27f351ed87a14545
IvanovAdrew <zyablik7771@gmail.com>
2019-03-12 22:20:29 +0200

    numberOfLines is how big massive we have, and Exception for >20 SYMBOLS

--- src/mainBlock/Main.java
+++ src/mainBlock/Main.java
@@ -6,30 +6,36 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         boolean b = true;
-        String[] arrayOfMyStrings = new String[20];//I use static massive because I don't need more lines
+        //I use static massive because I don't need more lines
         int numberOfLines = -1;
-        System.out.println("write how many lines you need, but not higher than 20");
+        System.out.println("write how many lines you need");
         while (b) {
             try {
-                try {
                     numberOfLines = Integer.parseInt(sc.nextLine());
-                    if (numberOfLines>20) {
-                        throw new StringTooLongException();
-                    } else{
-                        break;
-                    }
-                }catch (StringTooLongException e){
-                    System.out.println("it's more than 20!");
-                }
+                    break;
 
             }catch (NumberFormatException e) {
                 System.out.println("please write a number");
             }
         }
+
         if (numberOfLines >0) {
-            System.out.println("please write " + numberOfLines + " number of lines");
+            String[] arrayOfMyStrings = new String[numberOfLines];
+            System.out.println("please write " + numberOfLines + " number of lines, but it must not more than 20 symbols");
             for (int i = 1; i <= numberOfLines; i++) {
-                arrayOfMyStrings[i] = sc.nextLine();
+                while(b) {
+                    try {
+
+                        arrayOfMyStrings[i] = sc.nextLine();
+                        if (arrayOfMyStrings[i].length() > 20) {
+                            throw new StringTooLongException();
+                        } else {
+                            break;
+                        }
+                    } catch (StringTooLongException e) {
+                        System.out.println("it's more than 20 symbols!");
+                    }
+                }
             }
         }
         System.out.println("I'm here");
